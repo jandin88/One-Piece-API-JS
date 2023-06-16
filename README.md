65cc0c542f6f2d0973826eb64d45376124a3eec8
jandin88 <jandersson.coutinho@gmail.com>
2023-06-15 23:55:32 -0300

    update de imagens

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # One-Piece-API-JS
-Projeto simples para relembrar JavaScript e adquirir conhecimento sobre API
+Projeto simples para relembrar JavaScript e adquirir conhecimento sobre API com js
+APi usada do site https://api-onepiece.com/
+
+OverView https://jandin88.github.io/One-Piece-API-JS/
--- Script/script.js
+++ Script/script.js
@@ -9,20 +9,17 @@ fetch('https://api.api-onepiece.com/characters/').then(response => response.json
         document.body.appendChild(divMain)
         elementos(data, divMain)
         botao.addEventListener("click", () => {
+    
 
             if (primary.className !== "active") {
-
                 botao.innerHTML = "Sair da tripulação"
                 primary.setAttribute("class", "active")
                 divMain.setAttribute("class", "active")
-                console.log("entrou")
-
             }
             else {
                 primary.removeAttribute("class", "active")
                 divMain.removeAttribute("Class", "active")
                 botao.innerHTML = "Acessar a tripulação"
-                console.log("entrou3")
             }
 
         })
@@ -30,18 +27,16 @@ fetch('https://api.api-onepiece.com/characters/').then(response => response.json
 
 
 function elementos(data, divMain) {
-    let count=1;
+    
     data.map((item) => {
         if (item.id < 12) {
             const ul = document.createElement("ul")
             
-            ul.style.transition="20s"
-            
+           
             const liImg = document.createElement("li")
             liImg.setAttribute("class","fotos")
-            liImg.innerHTML='<img src="/img/images'+count+'.jpeg">'
-            count++
-
+            liImg.innerHTML='<img src="img/images'+item.id+'.jpeg">'
+           
             const liNome = document.createElement("li")
             liNome.textContent = "nome: " + item.french_name;
 
@@ -52,16 +47,16 @@ function elementos(data, divMain) {
             liAltura.textContent = "Altura: " + item.size;
 
             const liIdade = document.createElement("li")
-            liIdade.textContent = "Altura: " + item.age;
-
+            liIdade.textContent = "Altura: " + item.age;            
             ul.appendChild(liImg);
             ul.appendChild(liNome);
             ul.appendChild(liCargo);
             ul.appendChild(liAltura);
             ul.appendChild(liIdade);
 
+            
             divMain.appendChild(ul)
-        }
+                    }
     })
 
 
